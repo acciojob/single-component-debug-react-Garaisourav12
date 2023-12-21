fc5566e822366dc629b6c94446947c7118dfa6c6
lal15aditi <lal15aditi@gmail.com>
2023-12-21 19:27:17 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import './index.css'; // Optional: Import your global styles
 
 ReactDOM.render(
   <React.StrictMode>
